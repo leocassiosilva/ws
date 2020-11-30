5df3b81b42e8585893be6eaf3eaecfe81dd5ce7d
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-30 13:55:37 -0300

    novas

--- src/wsaula/PublishWS.java
+++ src/wsaula/PublishWS.java
@@ -1,11 +1,13 @@
 package wsaula;
 
+/*Responsavel por deixar o serviço disponivel*/
 import javax.xml.ws.Endpoint;
 
 /**
  *
  * @author Leocassio
  */
+/*Classe principal que vai ser responsavel por publicar o serviço*/
 public class PublishWS {
 
     public static void main(String[] args) {
