2cae4cf238aa94786b16b1ed36afe7f593c56acd
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-30 12:24:23 -0300

    first commit

---
+++ src/wsaula/WsAula.java
@@ -0,0 +1,16 @@
+package wsaula;
+
+/**
+ *
+ * @author Leocassio
+ */
+public class WsAula {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
